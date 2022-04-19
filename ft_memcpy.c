eaa8912c5d2c924dd3c4340983efd95efe20de7c
jseijo-p <jseijo-p@student.42malaga.com>
2022-04-19 16:55:14 +0200

    Done ft_memcpy for use with ft_strdup.

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jseijo-p <jseijo-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 10:11:47 by jseijo-p          #+#    #+#             */
-/*   Updated: 2022/04/19 11:11:42 by jseijo-p         ###   ########.fr       */
+/*   Updated: 2022/04/19 16:54:40 by jseijo-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,15 @@
 
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
+	size_t	i;
+
+	if (!dst && !src)
+		return (0);
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dst);
 }
