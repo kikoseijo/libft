1695d2a7d54f2a89d0ec24c7c66685ec519d5382
jseijo-p <jseijo-p@student.42malaga.com>
2022-04-19 19:52:01 +0200

    Done ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: jseijo-p <jseijo-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 10:11:41 by jseijo-p          #+#    #+#             */
-/*   Updated: 2022/04/19 11:11:37 by jseijo-p         ###   ########.fr       */
+/*   Updated: 2022/04/19 19:51:31 by jseijo-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,14 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((unsigned char *)s)[i] == (unsigned char)c)
+			return ((void *)s + i);
+		i++;
+	}
+	return (0);
 }
