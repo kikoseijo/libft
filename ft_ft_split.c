687daabb12e25e8eb54a21f41af7c42585c9f5e0
jseijo-p <jseijo-p@student.42malaga.com>
2022-04-20 17:42:43 +0200

    Done with failure ft_split.

--- ft_ft_split.c
+++ ft_ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jseijo-p <jseijo-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 10:16:59 by jseijo-p          #+#    #+#             */
-/*   Updated: 2022/04/19 10:17:20 by jseijo-p         ###   ########.fr       */
+/*   Updated: 2022/04/20 16:51:29 by jseijo-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,89 @@
 ** with a NULL pointer.
 */
 
+#include "libft.h"
+#include <stdio.h>
+
+static int	ft_wordcount(char *s, int sep)
+{
+	int	i;
+
+	i = 0;
+	while (*s)
+	{
+		while (*s == (char)sep)
+			s++;
+		if (!*s)
+			break ;
+		while (*s != (char)sep && *s)
+			s++;
+		i++;
+	}
+	return (i);
+}
+
+static int	ft_count_next_word_len(char *s, char c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (i);
+		i++;
+	}
+	return (i);
+}
+
 char	**ft_split(char const *s, char c)
 {
+	int		i;
+	int		j;
+	int		end;
+	char	**tab;
+
+	if (!s)
+		return (ft_calloc(0, sizeof(char *)));
+	tab = ft_calloc(ft_wordcount((char *)s, c) + 1, sizeof(char *));
+	if (!tab)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s[i] != '\0')
+	{
+		while (s[i] == c)
+			i++;
+		end = ft_count_next_word_len(&((char *)s)[i], c);
+		tab[j] = ft_substr(&((char *)s)[i], 0, end);
+		i++;
+		j++;
+	}
+	return (tab);
+}
+
+int	main(void)
+{
+	char	**trab;
+	int		i;
+
+	printf("split---------------------------------------\n");
+	trab = ft_split("111aki1yama11ryo111jiro1111", '1');
+	printf("ft_split(\"111aki1yama11ryo111jiro1111\", '1'):\n");
+	i = 0;
+	if (!trab)
+		printf("%p\n", trab);
+	else
+	{
+		while (1)
+		{
+			printf("[%d]:%s\n", i, trab[i]);
+			if (!trab[i])
+				break ;
+			free(trab[i]);
+			i++;
+		}
+	}
+	free(trab);
+	return (0);
 }
