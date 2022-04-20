ac9659612e2899754e61a12e592c4d29af7cffa2
jseijo-p <jseijo-p@student.42malaga.com>
2022-04-20 12:10:31 +0200

    Done ft_strtrim

--- ft_ft_strtrim.c
+++ ft_ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: jseijo-p <jseijo-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 10:16:13 by jseijo-p          #+#    #+#             */
-/*   Updated: 2022/04/19 10:16:28 by jseijo-p         ###   ########.fr       */
+/*   Updated: 2022/04/20 12:10:12 by jseijo-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,43 @@
 ** from the beginning and the end of the string.
 */
 
+#include "libft.h"
+
+static int	ft_char_in_set(char c, char const *set)
+{
+	int	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	int		i;
+	int		start;
+	int		len;
+	char	*res;
+
+	start = 0;
+	len = ft_strlen(s1);
+	while (s1[start] && ft_char_in_set(s1[start], set))
+		start++;
+	while (len > start && ft_char_in_set(s1[len - 1], set))
+		len--;
+	res = ft_calloc(len - start + 1, sizeof(char *));
+	i = 0;
+	while (start < len)
+	{
+		res[i] = s1[start];
+		start++;
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
 }
