5332813849bd5f6b1050bd291e6665917ad9c999
jseijo-p <jseijo-p@student.42malaga.com>
2022-04-20 13:12:49 +0200

    Done ft_substr.

--- ft_ft_substr.c
+++ ft_ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jseijo-p <jseijo-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 10:14:03 by jseijo-p          #+#    #+#             */
-/*   Updated: 2022/04/19 10:33:36 by jseijo-p         ###   ########.fr       */
+/*   Updated: 2022/04/20 13:12:30 by jseijo-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,21 @@
 ** maximum size ’len’.
 */
 
+#include "libft.h"
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
+	int		i;
+	char	*res;
+
+	i = 0;
+	res = ft_calloc(len + 1, sizeof(char *));
+	while (start < len)
+	{
+		res[i] = s[start];
+		start++;
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
 }
