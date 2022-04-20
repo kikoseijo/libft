1a553ca0cf706d877efc644e68ef26582a2095cf
jseijo-p <jseijo-p@student.42malaga.com>
2022-04-20 11:35:22 +0200

    Done ft_itoa.

--- ft_ft_itoa.c
+++ ft_ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: jseijo-p <jseijo-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 10:18:04 by jseijo-p          #+#    #+#             */
-/*   Updated: 2022/04/19 10:18:25 by jseijo-p         ###   ########.fr       */
+/*   Updated: 2022/04/20 11:35:01 by jseijo-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,49 @@
 ** Negative numbers must be handled.
 */
 
+#include "libft.h"
+
+static long	ft_abs(long n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
+
+static int	ft_nbrlen(long long int n)
+{
+	int	i;
+
+	i = 1;
+	if (n < 0)
+	{
+		i++;
+		n = -n;
+	}
+	while (n > 9)
+	{
+		n /= 10;
+		i++;
+	}
+	return (i);
+}
+
 char	*ft_itoa(int n)
 {
+	char	*res;
+	long	nb;
+	int		len;
+
+	len = ft_nbrlen(n);
+	res = ft_calloc(len, sizeof(char *));
+	nb = ft_abs(n);
+	res[len] = '\0';
+	while (len--)
+	{
+		res[len] = nb % 10 + '0';
+		nb /= 10;
+	}
+	if (n < 0)
+		res[0] = '-';
+	return (res);
 }
