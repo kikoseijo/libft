82b95268653aa912e634cb4617f3c7f349fd0b9d
Kiko Seijo <kiko@sunnyface.com>
2022-04-24 12:15:11 +0200

    Working on ft_lstmap.

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: jseijo-p <jseijo-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 10:31:35 by jseijo-p          #+#    #+#             */
-/*   Updated: 2022/04/24 10:54:40 by jseijo-p         ###   ########.fr       */
+/*   Updated: 2022/04/24 12:14:51 by jseijo-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,20 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list	*res;
+	t_list	*tmp;
+
+	if (!lst || !f || !del)
+		return ;
+	res = ft_lstnew(f(lst->content));
+	if (!res)
+		return (NULL);
+	= res;
+	lst = lst->next;
+	while (lst)
+	{
+		res->next = ft_lstnew(f(lst->content));
+	}
+	// TODO: here;
+	return (res);
 }
