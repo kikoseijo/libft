acd239bb52091f6e7dd041d1c76e9d93873bcea0
jseijo-p <jseijo-p@student.42malaga.com>
2022-04-25 09:00:36 +0200

    Done lstmap.

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: jseijo-p <jseijo-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 10:31:35 by jseijo-p          #+#    #+#             */
-/*   Updated: 2022/04/24 12:14:51 by jseijo-p         ###   ########.fr       */
+/*   Updated: 2022/04/25 08:59:54 by jseijo-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,16 +28,24 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*tmp;
 
 	if (!lst || !f || !del)
-		return ;
-	res = ft_lstnew(f(lst->content));
-	if (!res)
 		return (NULL);
-	= res;
+	tmp = ft_lstnew(f(lst->content));
+	if (!tmp)
+		return (NULL);
+	res = tmp;
 	lst = lst->next;
 	while (lst)
 	{
 		res->next = ft_lstnew(f(lst->content));
+		if (!res->next)
+		{
+			ft_lstclear(&tmp, del);
+			return (NULL);
+		}
+		res = res->next;
+		lst = lst->next;
+		// ft_lstdelone(lst, del);
 	}
-	// TODO: here;
+	res->next = NULL;
 	return (res);
 }
