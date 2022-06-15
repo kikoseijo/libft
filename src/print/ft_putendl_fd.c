e874d8b7820bf5a026007d1b6c5cb5a5903dfe96
jseijo-p <jseijo-p@student.42malaga.com>
2022-06-15 11:47:08 +0200

    Fixing wrong student header email.

--- src/print/ft_putendl_fd.c
+++ src/print/ft_putendl_fd.c
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft_putendl_fd.c                                 :+:      :+:    :+:   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jseijo-p <jseijo-p@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jseijo-p <jseijo-p@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 10:21:52 by jseijo-p          #+#    #+#             */
-/*   Updated: 2022/04/19 14:25:24 by jseijo-p         ###   ########.fr       */
+/*   Updated: 2022/06/15 11:46:02 by jseijo-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 ** Outputs the string ’s’ to the given file descriptor
 ** followed by a newline.
-** CHECK: when no string we write a new line allways. 
+** CHECK: when no string we write a new line allways.
 */
 
 #include "../../inc/libft.h"
